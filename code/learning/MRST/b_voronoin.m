e35104e986cf92808bfe01c69e8892743d046739
Andreas Berg <51373685+BollaBerg@users.noreply.github.com>
2022-03-19 15:04:38 +0100

    Add second example from the book

---
+++ code/learning/MRST/b_voronoin.m
@@ -0,0 +1,25 @@
+% EXAMPLE 2 (PAGE 10) OF Berge, Klemetsdal et.al.
+
+% Create flags for non-boundary cells
+keep = false(11, 11, 11);
+keep(2:10, 2:10, 1:11) = true;
+
+% Create sites to use for generating Voronoi grid
+[X, Y, Z] = meshgrid(linspace(0, 1, 11));
+% Scew the X-axis slightly, similar as it is done for the example
+% Note: I've tried scewing the Z-axis, but that produces (extremely) weird
+% plots - figure out why!
+sites = [X(:)+0.4*Y(:).^2 Y(:)+0.1*X(:).^2 Z(:)];
+
+% Use voronoin to create voronoi grid of sites
+[V, C] = voronoin(sites);
+% Keep only non-boundary cells
+C = C(keep(:));
+% Create a 3D MRST grid
+grid = voronoi2mrstGrid3D(V, C);
+
+% Plot figure
+figure
+plotGrid(grid); view(120, 25)
+axis equal tight off, camlight headlight
+set(gca,'DataAsp',[1 .7 2.5])
