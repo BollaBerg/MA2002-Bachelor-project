f009ed8847a26db3916af986a0c0f0fce6795b07
Andreas Berg <51373685+BollaBerg@users.noreply.github.com>
2022-03-30 14:20:11 +0200

    Add example 4

---
+++ code/learning/MRST/d_clippedPebi2D.m
@@ -0,0 +1,21 @@
+% EXAMPLE 4 (PAGE 11) OF Berge, Klemetsdal et.al.
+
+% Start by defining the boundary as a polygon
+% We create something that looks like a house, just for fun
+boundary = [
+    0, 0;
+    1, 0;
+    1, 1;
+    0.5, 1.2;
+    0, 1;
+];
+
+% Create random sites
+sites = rand(30, 2);
+
+% Generate Voronoi grid
+G = clippedPebi2D(sites, boundary);
+
+% Plot
+figure();
+plotGrid(G);
