a667c46a7c563ac106bb4566a75af0595ccb3145
Andreas Berg <51373685+BollaBerg@users.noreply.github.com>
2022-05-18 19:50:43 +0200

    Add (non-working) clippedPebi2DGmsh method
    The method generates G using pebi_grid_2D.py, then uses clippedPebi2D
    with G.nodes.coords to generate a clipped PEBI grid.
    
    It currently fails due to the following error:
    clippedPebi2D assumes that each cell has 1 or 2 neighbors (i.e.
        neigh = G.cells.faces==i;
        sum(neigh) == 1 or sum(neigh) == 2
    In our case, we have one single entry where sum(neigh) == 3. I do
    not yet know if the error is on our side (i.e. generating a wrong
    grid), or if it is on clippedPebi2Ds side (i.e. it should handle
    cases where sum(neigh) > 2.

---
+++ code/gmsh_to_mrst/exampleClippedPebi2DGmsh.m
@@ -0,0 +1,29 @@
+%% Example of pebiGrid2DGmsh usage
+% Create a simple 2D mesh using Gmsh, called from MATLAB
+
+% Setup required variables
+resGridSize = 0.25;
+domain = [1 1];
+faceConstraints.a.x = [0.25 0.4 0.75];
+faceConstraints.a.y = [0.25 0.5 0.75];
+faceConstraints.b.x = [0.8 0.9];
+faceConstraints.b.y = [0.1 0.2];
+faceConstraints.theseCanHaveAnyNames.x = [0.2 0.9];
+faceConstraints.theseCanHaveAnyNames.y = [0.9 0.1];
+
+% pebiGrid2DGmsh returns a single variable, G
+G = clippedPebi2DGmsh(resGridSize, domain, faceConstraints);
+
+% Plot result
+hold on;
+plotGrid(G, 'faceColor', 'none');
+plot(G.nodes.coords(:, 1), G.nodes.coords(:, 2), LineStyle='none', Marker='o')
+
+% Plot lines
+plot(faceConstraints.a.x, faceConstraints.a.y, 'lineWidth', 2, 'color', 'magenta', 'markersize', 3);
+plot(faceConstraints.b.x, faceConstraints.b.y, 'lineWidth', 2, 'color', 'magenta', 'markersize', 3);
+plot(faceConstraints.theseCanHaveAnyNames.x, faceConstraints.theseCanHaveAnyNames.y, 'lineWidth', 2, 'color', 'magenta', 'markersize', 3);
+
+% Save plot
+f = gcf;
+exportgraphics(f,'exampleClippedPebi2DGmsh.png')
