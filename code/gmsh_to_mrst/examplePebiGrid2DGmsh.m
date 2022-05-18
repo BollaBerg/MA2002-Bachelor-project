92c61e01bb7a55002a0515a5911c9599cc790f8e
Andreas Berg <51373685+BollaBerg@users.noreply.github.com>
2022-05-18 17:33:15 +0200

    Add another line to example
    File now runs, but only on laptop. Requires package 'gmsh' in
    installed Python packages, but is updated to run on Python 3.8

--- code/gmsh_to_mrst/examplePebiGrid2DGmsh.m
+++ code/gmsh_to_mrst/examplePebiGrid2DGmsh.m
@@ -4,10 +4,12 @@
 % Setup required variables
 resGridSize = 0.25;
 domain = [1, 1];
-faceConstraints.a.x = [0.25 0.75];
-faceConstraints.a.y = [0.25 0.75];
-faceConstraints.b.x = [0.1 0.2];
+faceConstraints.a.x = [0.25 0.4 0.75];
+faceConstraints.a.y = [0.25 0.5 0.75];
+faceConstraints.b.x = [0.8 0.9];
 faceConstraints.b.y = [0.1 0.2];
+faceConstraints.theseCanHaveAnyNames.x = [0.2 0.9];
+faceConstraints.theseCanHaveAnyNames.y = [0.9 0.1];
 
 % pebiGrid2DGmsh returns a single variable, G
 G = pebiGrid2DGmsh(resGridSize, domain, faceConstraints);
@@ -15,7 +17,11 @@ G = pebiGrid2DGmsh(resGridSize, domain, faceConstraints);
 % Plot result
 hold on;
 plotGrid(G, 'faceColor', 'none');
-plotLinePath(faceConstrants, 'lineWidth', 2, 'color', 'magenta', 'markersize', 3);
+
+% Plot lines
+plot(faceConstraints.a.x, faceConstraints.a.y, 'lineWidth', 2, 'color', 'magenta', 'markersize', 3);
+plot(faceConstraints.b.x, faceConstraints.b.y, 'lineWidth', 2, 'color', 'magenta', 'markersize', 3);
+plot(faceConstraints.theseCanHaveAnyNames.x, faceConstraints.theseCanHaveAnyNames.y, 'lineWidth', 2, 'color', 'magenta', 'markersize', 3);
 
 % Save plot
 f = gcf;
