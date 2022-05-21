f7a4299383a6434b1a8f6e41824df48a21ec56ca
Andreas Berg <51373685+BollaBerg@users.noreply.github.com>
2022-05-21 13:52:30 +0200

    Add mesh refinement, changing mesh size depending on distance to fractures
    Also enables cell_dimensions, letting the user input the default
    size of each cell

--- code/gmsh_to_mrst/pebi_grid_2D.py
+++ code/gmsh_to_mrst/pebi_grid_2D.py
@@ -11,19 +11,21 @@ Arguments:
         'faceConstraints' in pebiGrid2D in UPR.
 
 Output:
-    - Gmsh mesh saved to file `_pebi_grid_2D.m`
+    - Gmsh mesh saved to file TEMP_Gmsh_MRST.m`
 """
 from array import array
-import sys
 from typing import Union
 
 import gmsh
 
 def pebi_grid_2D(cell_dimensions: float, size: list,
-                lines: Union['list[list]', 'dict[str, list]', 'dict[str, dict[str, list]]']):
+                lines: Union['list[list]', 'dict[str, list]', 'dict[str, dict[str, list]]'],
+                _run_frontend: bool = False):
     # Do some (massive) argument handling, for use in MATLAB
     if isinstance(lines, dict):
-        if isinstance(list(lines.values())[0], array):
+        if len(lines) == 0:
+            lines = []
+        elif isinstance(list(lines.values())[0], array):
             # If data is sent from MATLAB, then lines is a dict
             # If there is only one line, then we accept it as a 1D dict, i.e.
             # lines is a dict in the shape {x: array, y: array}
@@ -95,36 +97,64 @@ def pebi_grid_2D(cell_dimensions: float, size: list,
     # Embed all fractures
     gmsh.model.mesh.embed(1, fractures, 2, surface)
 
+    # Refine mesh around fractures. We want a finer mesh around the fractures,
+    # to ensure as close fit of faces as possible. In addition, this helps
+    # handle intersecting fractures
+    # This is gotten from
+    # https://gitlab.onelab.info/gmsh/gmsh/-/blob/master/tutorials/python/t10.py
+
+    # The Distance field returns the distance to (sampling) points on each
+    # fracture
+    sampling = 100
+    gmsh.model.mesh.field.add("Distance", 1)
+    gmsh.model.mesh.field.setNumbers(1, "CurvesList", fractures)
+    gmsh.model.mesh.field.setNumber(1, "Sampling", sampling)
+
+    # The Treshold field uses the value from the Distance field to define a
+    # change in element size depending on the computed distances
+    min_factor = 1 / 2
+    min_distance = 0.05
+    max_distance = 0.2
+    gmsh.model.mesh.field.add("Threshold", 2)
+    gmsh.model.mesh.field.setNumber(2, "InField", 1)
+    gmsh.model.mesh.field.setNumber(2, "SizeMin", cell_dimensions * min_factor)
+    gmsh.model.mesh.field.setNumber(2, "SizeMax", cell_dimensions)
+    gmsh.model.mesh.field.setNumber(2, "DistMin", min_distance)
+    gmsh.model.mesh.field.setNumber(2, "DistMax", max_distance)
+
+    # We can use several fields here, see link above
+    # We can then select the minimum of all fields
+
+    # As we currently only have the treshold field, we use this as mesh
+    gmsh.model.mesh.field.setAsBackgroundMesh(2)
+
+    # As we define the entire element size in our background mesh, we disable
+    # certain on-by-default mesh calculations
+    gmsh.option.setNumber("Mesh.MeshSizeExtendFromBoundary", 0)
+    gmsh.option.setNumber("Mesh.MeshSizeFromPoints", 0)
+    gmsh.option.setNumber("Mesh.MeshSizeFromCurvature", 0)
+
+    # We finally set the meshing algorithm to Delaunay.
+    # According to the link above, Frontal-Delaunay (6) usually leads to the
+    # highest quality meshes, but Delaunay (5) handles complex mesh sizes
+    # better - especially size fields with large element size gradients
+    gmsh.option.setNumber("Mesh.Algorithm", 5)
+
     # Generate 2D mesh
     gmsh.model.mesh.generate(2)
 
-    # Save to disk
-    gmsh.write("TEMP_Gmsh_MRST.m")
+    if _run_frontend:
+        gmsh.fltk.run()
+    else:
+        # Save to disk
+        gmsh.write("TEMP_Gmsh_MRST.m")
 
     # Always finalize
     gmsh.finalize()
 
-
 if __name__ == "__main__":
-    try:
-        cell_dimensions = sys.argv[1]
-    except IndexError:
-        cell_dimensions = 0.25
-        print(f"No cell dimensions found. Default to {cell_dimensions}")
-
-    try:
-        size = sys.argv[2]
-    except IndexError:
-        size = [1, 1]
-        print(f"No size found. Default to {size}")
-    if len(size) < 2:
-        raise ValueError(f"Size must have length >= 2. Current length: {len(size)}")
-
-    
-    try:
-        lines = sys.argv[3]
-    except IndexError:
-        lines = []
-        print("No lines found. Default to empty list.")
-    
-    pebi_grid_2D(cell_dimensions, size, lines)
+    pebi_grid_2D(0.1, [1, 1], [
+            [[0.2, 0.5], [0.8, 0.5]],
+            [(0.5, 0.2), (0.5, 0.8)],
+        ],
+        _run_frontend=True)
