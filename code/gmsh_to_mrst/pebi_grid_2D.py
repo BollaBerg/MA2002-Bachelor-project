5cf167d8212ad87c4a90f454567580f308cdc6b2
Andreas Berg <51373685+BollaBerg@users.noreply.github.com>
2022-05-21 16:45:37 +0200

    Restructure code. Add support for single-point fractures

--- code/gmsh_to_mrst/pebi_grid_2D.py
+++ code/gmsh_to_mrst/pebi_grid_2D.py
@@ -18,6 +18,36 @@ from typing import Union
 
 import gmsh
 
+
+def _assert_column_in_dict(lines: dict, column: str):
+    if column not in lines.keys():
+        raise ValueError(
+            f"Dictionary {dict} must contain column `{column}`!"
+        )
+
+def _assert_columns_have_same_length(lines: dict, col1: str, col2: str):
+    if len(lines.get(col1)) != len(lines.get(col2)):
+        raise ValueError(
+            f"Column `{col1}` and `{col2}` must be the same length! "
+            + f"len({col1}): {len(lines.get(col1))}, len({col2}): {len(lines.get(col2))}"
+        )
+
+def _check_array_dict_and_return_line_list(lines: dict):
+    # Check that lines contains column x and y
+    _assert_column_in_dict(lines, "x")
+    _assert_column_in_dict(lines, "y")
+    
+    # Handle actual content
+    if isinstance(lines.get("x"), float):
+        # If the 'line' is a single point, then x and y are floats,
+        # and must be handled explicitly
+        return [(lines.get("x"), lines.get("y"))]
+    else:
+        # Assume 'line' is a list/tuple/Iterable of floats
+        _assert_columns_have_same_length(lines, "x", "y")
+        return list(zip(lines.get("x"), lines.get("y")))
+
+
 def pebi_grid_2D(cell_dimensions: float, size: list,
                 lines: Union['list[list]', 'dict[str, list]', 'dict[str, dict[str, list]]'],
                 savename: str = "TEMP_Gmsh_MRST.m",
@@ -25,32 +55,32 @@ def pebi_grid_2D(cell_dimensions: float, size: list,
     # Do some (massive) argument handling, for use in MATLAB
     if isinstance(lines, dict):
         if len(lines) == 0:
+            # Lines is an empty list -> No fractures to handle, simply create
+            # a single mesh within size
             lines = []
         elif isinstance(list(lines.values())[0], array):
             # If data is sent from MATLAB, then lines is a dict
             # If there is only one line, then we accept it as a 1D dict, i.e.
-            # lines is a dict in the shape {x: array, y: array}
-            if "x" not in lines.keys() or "y" not in lines.keys():
-                raise ValueError(
-                    "If lines is a single-level dict, it must have keys x, y. "
-                    + f"Current keys: {lines.keys()}"
-                )
-            lines = list(zip(lines.get("x"), lines.get("y")))
+            # one can write
+            #   faceConstraints.x = [x1 ...];
+            #   faceConstraints.y = [y1 ...];
+            # Then lines is a dict in the shape {x: array, y: array}
+            lines = _check_array_dict_and_return_line_list(lines)
 
         elif isinstance(list(lines.values())[0], dict):
             # If data is sent from MATLAB, then lines is a dict of dicts
+            # If there are more than one line, then it must be a 2D dict, i.e.
+            # one can write
+            #   faceConstraints.line1.x = [x11 ...];
+            #   faceConstraints.line1.y = [y11 ...];
+            #   faceConstraints.line2.x = [x21 ...];
+            #   faceConstraints.line2.y = [y21 ...];
             # Each element in lines.values is a dict of {x: array, y: array}
             new_lines = []
             for line in lines.values():
-                if "x" not in line.keys() or "y" not in line.keys():
-                    raise ValueError(
-                        "If lines is a 2D dict, then its values must have the "
-                        + "keys x, y. "
-                        + f"Current keys: {lines.keys()}, current lines: {lines}"
-                    )
-                new_lines.append(list(zip(line.get("x"), line.get("y"))))
+                new_lines.append(_check_array_dict_and_return_line_list(line))
             lines = new_lines
-
+    
     gmsh.initialize()
     gmsh.model.add("pebiGrid2D")
 
@@ -76,14 +106,16 @@ def pebi_grid_2D(cell_dimensions: float, size: list,
     # Define surface from circumference
     surface = gmsh.model.geo.add_plane_surface([circumference])
 
+    fracture_points = []
     fractures = []
     # Create fracture points
     for line in lines:
-        if len(line) < 2:
-            # No lines, only points
-            raise NotImplementedError(
-                f"Single-point fractures not implemented yet. Fracture: {line}"
+        # Handle single points their own way
+        if len(line) == 1:
+            fracture_points.append(
+                gmsh.model.geo.add_point(line[0][0], line[0][1], 0)
             )
+            continue
         
         # There exists a line with at least 1 segment
         # As line segment [i] starts with the end of line segment [i - 1],
@@ -101,6 +133,7 @@ def pebi_grid_2D(cell_dimensions: float, size: list,
 
     # Embed all fractures
     gmsh.model.mesh.embed(1, fractures, 2, surface)
+    gmsh.model.mesh.embed(0, fracture_points, 2, surface)
 
     # Refine mesh around fractures. We want a finer mesh around the fractures,
     # to ensure as close fit of faces as possible. In addition, this helps
@@ -112,12 +145,13 @@ def pebi_grid_2D(cell_dimensions: float, size: list,
     # fracture
     sampling = 100
     gmsh.model.mesh.field.add("Distance", 1)
+    gmsh.model.mesh.field.setNumbers(1, "PointsList", fracture_points)
     gmsh.model.mesh.field.setNumbers(1, "CurvesList", fractures)
     gmsh.model.mesh.field.setNumber(1, "Sampling", sampling)
 
     # The Treshold field uses the value from the Distance field to define a
     # change in element size depending on the computed distances
-    min_factor = 1 / 2
+    min_factor = 1 / 3
     min_distance = 0.05
     max_distance = 0.2
     gmsh.model.mesh.field.add("Threshold", 2)
@@ -161,8 +195,9 @@ def pebi_grid_2D(cell_dimensions: float, size: list,
 
 if __name__ == "__main__":
     pebi_grid_2D(0.1, [1, 1], [
-            [[0.2, 0.5], [0.8, 0.5]],
-            [(0.5, 0.2), (0.5, 0.5), (0.5, 0.8)],
+            [(0.25, 0.25), (0.4, 0.5), (0.75, 0.75)],
+            [(0.8, 0.1), (0.9, 0.2)],
+            [(0.2, 0.9), (0.9, 0.1)]
         ],
         savename=None,
         _run_frontend=True)
